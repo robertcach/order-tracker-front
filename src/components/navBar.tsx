7070cfef4e9368cfe4af7c1039fd139c1abc1055
Robert Cabrera <cabrera.rch@gmail.com>
2023-07-24 18:00:38 +0200

    style: add basic stlyes no nav links

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -3,10 +3,16 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <nav className="col-span-2">
-      <ul className="flex flex-col">
+    <nav className="h-full col-span-2 text-white bg-primary">
+      <ul className="flex flex-col gap-3 p-3 ">
         {NAV_LINKS.map((link) => (
-          <Link href={link.route}>{link.label}</Link>
+          <Link
+            key={link.label}
+            className="px-2 py-1 border-2 rounded-md bg-secondary border-secondary hover:border-2 hover:border-white"
+            href={link.route}
+          >
+            {link.label}
+          </Link>
         ))}
       </ul>
     </nav>
