4ae71416f1a5e3169b6f61c18f59b7cb2fe939d5
Robert Cabrera <cabrera.rch@gmail.com>
2023-07-29 18:49:03 +0200

    feat: update component with new prop to showPrice or not

--- src/components/orderProductCard.tsx
+++ src/components/orderProductCard.tsx
@@ -3,9 +3,13 @@ import Image from "next/image";
 
 interface OrderProductCardProps {
   product: Product;
+  showPrice?: boolean;
 }
 
-export default function OrderProductCard({ product }: OrderProductCardProps) {
+export default function OrderProductCard({
+  product,
+  showPrice,
+}: OrderProductCardProps) {
   return (
     <div className="flex gap-4">
       <Image
@@ -19,6 +23,7 @@ export default function OrderProductCard({ product }: OrderProductCardProps) {
         <p>{product.title}</p>
         <p>{product.price}</p>
       </div>
+      {showPrice && <p className="ml-auto">{product.price}€</p>}
     </div>
   );
 }
