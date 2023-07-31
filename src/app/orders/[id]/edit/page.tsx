bf953ef9626e4ed89eb97f06b6b4a993fd4de46b
Robert Cabrera <cabrera.rch@gmail.com>
2023-07-31 20:18:49 +0200

    feat: add useEffect to set state from order's data to use that data to edit form

--- src/app/orders/[id]/edit/page.tsx
+++ src/app/orders/[id]/edit/page.tsx
@@ -1,3 +1,16 @@
-export default function EditOrder() {
+"use client";
+
+import { useGetOrder } from "@/hooks/useGetOrder";
+import { Order } from "@/interfaces";
+import { useEffect, useState } from "react";
+
+export default function EditOrder({ params }: any) {
+  const [order, setOrder] = useState<Order | undefined>(undefined);
+  const { id } = params;
+
+  useEffect(() => {
+    useGetOrder(id).then((order) => setOrder(order));
+  }, []);
+
   return <p>Edit order</p>;
 }
