23c15bc609bb66022823faf3569a4528364e1d2b
Robert Cabrera <cabrera.rch@gmail.com>
2023-07-31 19:48:24 +0200

    style: add styles to product's listbox selector

--- src/app/orders/new/page.tsx
+++ src/app/orders/new/page.tsx
@@ -2,8 +2,9 @@
 import { CUSTOMERS_LIST, PRODUCTS_LIST } from "@/constans";
 import { useCreateOrder } from "@/hooks/useCreateOrder";
 import { NewOrderBody } from "@/interfaces";
-import { Listbox } from "@headlessui/react";
-import { useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import { Fragment, useState } from "react";
 import { useForm } from "react-hook-form";
 
 const defaultStatus = "Pending";
@@ -52,26 +53,73 @@ export default function NewOrder() {
           </select>
         </div>
 
-        <div>
+        <div className="flex items-center gap-2">
           <label htmlFor="productsID">Products</label>
-          <Listbox
-            value={selectedProducts}
-            onChange={setSelectedProducts}
-            multiple
-          >
-            <Listbox.Button>
-              {selectedProducts.length
-                ? selectedProducts.map((person) => person.name).join(", ")
-                : "Ver"}
-            </Listbox.Button>
-            <Listbox.Options>
-              {PRODUCTS_LIST.map((product) => (
-                <Listbox.Option key={product.id} value={product}>
-                  {product.name}
-                </Listbox.Option>
-              ))}
-            </Listbox.Options>
-          </Listbox>
+          <div className="w-72">
+            <Listbox
+              value={selectedProducts}
+              onChange={setSelectedProducts}
+              multiple
+            >
+              <div className="relative mt-1">
+                <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                  <span className="block truncate">
+                    {selectedProducts.length
+                      ? selectedProducts.map((person) => person.name).join(", ")
+                      : "Elegir productos"}
+                  </span>
+                  <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                    <ChevronUpDownIcon
+                      className="w-5 h-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </span>
+                </Listbox.Button>
+                <Transition
+                  as={Fragment}
+                  leave="transition ease-in duration-100"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                    {PRODUCTS_LIST.map((product) => (
+                      <Listbox.Option
+                        key={product.id}
+                        value={product}
+                        className={({ active }) =>
+                          `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                            active
+                              ? "bg-amber-100 text-amber-900"
+                              : "text-gray-900"
+                          }`
+                        }
+                      >
+                        {({ selected }) => (
+                          <>
+                            <span
+                              className={`block truncate ${
+                                selected ? "font-medium" : "font-normal"
+                              }`}
+                            >
+                              {product.name}
+                            </span>
+                            {selected ? (
+                              <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                <CheckIcon
+                                  className="w-5 h-5"
+                                  aria-hidden="true"
+                                />
+                              </span>
+                            ) : null}
+                          </>
+                        )}
+                      </Listbox.Option>
+                    ))}
+                  </Listbox.Options>
+                </Transition>
+              </div>
+            </Listbox>
+          </div>
         </div>
 
         <button>Create Product</button>
